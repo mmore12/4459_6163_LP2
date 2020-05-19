a7536c7068ec206de356988404b9eb54d23e12b8
mmore12 <a6163@alunos.ipca.pt>
2020-05-19 22:15:19 +0100

    classe animal

--- PetLovers/Animal.cs
+++ PetLovers/Animal.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 
 namespace PetLovers
@@ -23,6 +24,52 @@ namespace PetLovers
     public class Animal
     {
         #region Attributes
+        int id;
+        string nome, raca, genero;
+        DateTime data_Nasc;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Propertie to manage the animal ID
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        /// <summary>
+        /// Propertie to manage the name of animal
+        /// </summary>
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        /// <summary>
+        /// Propertie to manage the breed of animal
+        /// </summary>
+        public string Raca
+        {
+            get { return raca; }
+            set { raca = value; }
+        }
+        /// <summary>
+        /// Propertie to manage the animal genre
+        /// </summary>
+        public string Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+        /// <summary>
+        /// Propertie to manage the animal birth date
+        /// </summary>
+        public DateTime Data_Nasc
+        {
+            get { return data_Nasc; }
+            set { data_Nasc = value; }
+        }
         #endregion
 
         #region Methods
