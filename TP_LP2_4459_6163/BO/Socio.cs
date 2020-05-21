f4a42a6e76adb67099753b25b339379f1e9a8f64
elden <elden.carones@gmail.com>
2020-05-21 22:02:28 +0100

    error correction code

---
+++ TP_LP2_4459_6163/BO/Socio.cs
@@ -0,0 +1,104 @@
+﻿/*
+*	<copyright file="BO.cs" company="IPCA">
+*		Copyright (c) 2020 All Rights Reserved
+*	</copyright>
+* 	<author>CaronesE</author>
+*   <date>5/19/2020 11:09:39 PM</date>
+*	<description></description>
+**/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BO
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: CaronesE
+    /// Created on: 5/19/2020 11:09:39 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Socio : Pessoa
+    {
+        #region Attributes
+        static int totSocios;
+
+        DateTime dataInsc;
+        int idSocio;
+        int categoria;
+
+        enum CATEGORIA {
+            JUNIOR,
+            SENIOR,
+            VOLUNTARIO
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+
+        static Socio()
+        {
+            totSocios = 0;
+        }
+
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Socio()
+        {
+            idSocio = ++totSocios;
+        }
+
+        public Socio(DateTime dataInsc, string pNome, string uNome, DateTime dataNasc) : base(pNome, uNome, dataNasc)
+        {
+            idSocio = ++totSocios;
+            // CATEGORIA deve ser implementada numa validação
+            // 3 categorias que definirão mais tarde o preços das quotas.
+            //categoria sera deverá chamar uma funcão que implementa a categoria do método.
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DateTime DataInsc {
+            get { return dataInsc; }
+            set { dataInsc = value; }
+        }
+
+        public int IDSocio {
+            get { return idSocio; }
+        }
+
+        public int Categoria {
+            get { return categoria; }
+        }
+    
+
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Overrides
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Socio()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ TP_LP2_4459_6163/DAL/DadosParque.cs
@@ -0,0 +1,97 @@
+﻿using BO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DadosParque
+    {
+
+        private static List<Cao> caes;
+
+
+        //private static ArrayList caes;
+        private static ArrayList boletins;
+        private static ArrayList vacinas;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static DadosParque()
+        {
+
+            caes = new List<Cao>();
+
+            //caes = new ArrayList();
+            boletins = new ArrayList();
+            vacinas = new ArrayList();
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool AddCao(Cao c)
+        {
+            foreach (Cao cao in caes)
+            {
+                //valida se o cao já existe
+                if(cao.Id == c.Id)
+                { 
+                    return false; 
+                }
+            }
+            caes.Add(c);
+
+            return true;
+        }
+
+
+        
+        /// <summary>
+        /// Adicionar uma vacina
+        /// </summary>
+        /// <param name="vacina"></param>
+        /// <returns></returns>
+        public static bool AddVacina(Vacina vacina)
+        {
+            foreach (Vacina vacina1 in vacinas)
+            {
+                //valida se a vacina já existe
+                if (vacina1.Id == vacina.Id)
+                {
+                    return false;
+                }
+            }
+            vacinas.Add(vacina);
+            return true;
+        }
+        /// <summary>
+        /// Adicionar um boletim sanitario
+        /// </summary>
+        /// <param name="boletim"></param>
+        /// <returns></returns>
+        public static bool AddBoletim(BoletimSanitario bol)
+        {
+            foreach (BoletimSanitario boletimSanitario in boletins)
+            {
+                //valida se o boletim daquele animal já existe
+                if (boletimSanitario.Id_Animal == bol.Id_Animal)
+                {
+                    return false;
+                }
+            }
+            boletins.Add(bol);
+
+            return true;
+        }
+    }
+}
